9bfe5bc9c1ac829dcf7bcaf32a940832546b9d36
EnvasChan <envasenchan@gmail.com>
2020-08-12 17:50:45 +0900

    Yet i can't solve this problem, acm16991

--- project_hw/TSP_200811.cpp
+++ project_hw/TSP_200811.cpp
@@ -4,29 +4,42 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cmath>
 
 using namespace std;
 
 int n = 0;
 int a = 0;
-vector<double> loc[1001] = { 0 };
+vector<double> loc[1001];
 int m_dis = 32000;
+bool gate[1001] = { false };
 
-int tsp(int node, double dis, int num, int start_node)
+void tsp(int node, double dis, int num, int start_node)
 {
-	double final_dis = dis + sqrt((loc[node][0] - loc[start_node][0])^2 + 
-		(loc[node][1] - loc[start_node][1]) ^ 2);
-	if (node == n)
+	if (node == n-1)
 	{
+		double final_dis = dis + sqrt(pow(loc[node][0] - loc[start_node][0], 2) +
+			pow(loc[node][1] - loc[start_node][1], 2));
 		if (m_dis > final_dis)
 		{
-			m_dis + final_dis;
+			m_dis = final_dis;
 		}
+		return;
 	}
+
 
 	for (int i = 0; i < n; i++)
 	{
 
+		if (gate[i] == true)
+		{
+			continue;
+		}
+
+		gate[i] = true;
+		tsp(i, dis + sqrt(pow(loc[node][0] - loc[i][0], 2) +
+			pow(loc[node][1] - loc[i][1], 2)), num + 1, start_node);
+		gate[i] = false;
 	}
 }
 
@@ -51,9 +64,18 @@ int main()
 		}
 	}
 
+	for (int i = 0; i < n; i++)
+	{
+		gate[i] = true;
+		tsp(i, 0, 0, i);
+		gate[i] = false;
+	}
+	cout << m_dis;
+
+
 }
 
 
 
-#endif TSP_200811
+#endif
 
